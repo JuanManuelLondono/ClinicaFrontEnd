06bebe94f321153ba09390aa7080b0637b93a17c
JuanManuelLondono <juanm.londonom@uqvirtual.edu.co>
2023-11-09 22:03:25 -0500

    first commit

---
+++ src/app/pagina/registro/registro.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { RegistroPacienteDTO } from 'src/app/modelo/registro-paciente-dto';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+
+
+export class RegistroComponent {
+
+  
+  archivos!:FileList;
+  registroPacienteDTO: RegistroPacienteDTO;
+  ciudades:string[];
+constructor(){
+  this.ciudades = [];
+    this.cargarCiudades();
+this.registroPacienteDTO = new RegistroPacienteDTO();
+}
+
+public registrar(){
+  if(this.archivos != null && this.archivos.length > 0){
+  console.log(this.registroPacienteDTO);
+  }else{
+  console.log("Debe cargar una foto");
+  }
+  }
+
+  public sonIguales():boolean{
+    return this.registroPacienteDTO.password == this.registroPacienteDTO.confirmaPassword;
+    }
+
+
+private cargarCiudades(){
+  this.ciudades.push("Armenia");
+  this.ciudades.push("Calarcá");
+  this.ciudades.push("Pereira");
+  this.ciudades.push("Manizales");
+  this.ciudades.push("Medellín");
+  }
+
+  public onFileChange(event:any){
+    if (event.target.files.length > 0) {
+    const files = event.target.files;
+    console.log(files);
+    }
+    }
+
+}
+
