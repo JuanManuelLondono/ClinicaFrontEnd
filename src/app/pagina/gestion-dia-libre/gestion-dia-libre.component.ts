7157eb35b687c792e79431dd051485a08f8b99e4
Esteban-02 <OESTEBAN02@GMAIL.COM>
2023-11-19 00:07:58 -0500

    gestionDialibre

--- src/app/pagina/gestion-dia-libre/gestion-dia-libre.component.ts
+++ src/app/pagina/gestion-dia-libre/gestion-dia-libre.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { AgendarDiaLibreDTO } from 'src/app/modelo/agendar-dia-libre-dto';
+import { Alerta } from 'src/app/modelo/alerta';
+import { MedicoService } from 'src/app/servicios/medico.service'
+import { TokenService } from 'src/app/servicios/token.service';
+
 
 @Component({
   selector: 'app-gestion-dia-libre',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./gestion-dia-libre.component.css']
 })
 export class GestionDiaLibreComponent {
+
+  agendarDiaLibreDTO: AgendarDiaLibreDTO;
+  alerta!: Alerta;
+
+  constructor(private medicoService : MedicoService, private tokenService: TokenService){
+    this.agendarDiaLibreDTO = new AgendarDiaLibreDTO;
+  }
+
+  public agendarDiaLibre() {
+    let codigo = this.tokenService.getCodigo();
+
+    this.agendarDiaLibreDTO.codigoMedico = codigo;
+
+    
+    this.medicoService.agendarDiaLibre(this.agendarDiaLibreDTO).subscribe({
+        next: data => {
+          alert("Dia agendado con exito")
+          this.alerta = { tipo: "success", mensaje: data.respuesta }
+        },
+        error: data => {
+          this.alerta = { tipo: "danger", mensaje: data.error.respuesta }
+        }
+      })
+
+  }
 
 }
